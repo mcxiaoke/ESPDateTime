2bedefdfffa2952bc69431f1bf43c95ec63cafa9
mcxiaoke <mcxiaoke@live.com>
2021-03-18 23:35:06 +0800

    fix simple example timezone

--- examples/simple/main.cpp
+++ examples/simple/main.cpp
@@ -5,8 +5,8 @@
 #elif defined(ESP32)
 #include <WiFi.h>
 #endif
-#include "config.h"
 #include "ESPDateTime.h"
+#include "config.h"
 
 // don't forget to change this to real ssid/password
 // or set these in config.h and include it
@@ -37,7 +37,8 @@ void setupDateTime() {
   // DateTime.setTimeZone(-4);
   //   DateTime.setServer("asia.pool.ntp.org");
   //   DateTime.begin(15 * 1000);
-  DateTime.setTimeZone(8);
+  DateTime.setServer("ntp.aliyun.com");
+  DateTime.setTimeZone("CST-8");
   DateTime.begin();
   if (!DateTime.isTimeValid()) {
     Serial.println("Failed to get time from server.");
@@ -50,14 +51,30 @@ void setupDateTime() {
  *
  */
 
+void showTime() {
+  Serial.printf("TimeZone:      %s\n", DateTime.getTimeZone());
+  Serial.printf("Up     Time:   %lu seconds\n", millis() / 1000);
+  Serial.printf("Now    Time:   %ld\n", DateTime.now());
+  Serial.printf("Local  Time:   %s\n",
+                DateTime.format(DateFormatter::SIMPLE).c_str());
+  Serial.printf("ISO86  Time:   %s\n", DateTime.toISOString().c_str());
+  Serial.printf("UTC    Time:   %s\n",
+                DateTime.formatUTC(DateFormatter::SIMPLE).c_str());
+  Serial.printf("UTC86  Time:   %s\n",
+                DateTime.formatUTC(DateFormatter::ISO8601).c_str());
+
+  Serial.println("===========");
+  time_t t = time(NULL);
+  Serial.printf("OS local:     %s", asctime(localtime(&t)));
+  Serial.printf("OS UTC:       %s", asctime(gmtime(&t)));
+}
+
 void setup() {
   delay(1000);
   Serial.begin(115200);
   setupWiFi();
   setupDateTime();
   Serial.println(DateTime.now());
-  Serial.println(DateTime.getTime());
-  Serial.println(DateTime.utcTime());
   Serial.println("--------------------");
   Serial.println(DateTime.toString());
   Serial.println(DateTime.toISOString());
@@ -68,30 +85,22 @@ void setup() {
   Serial.println(DateTime.format(DateFormatter::TIME_ONLY));
   Serial.println("--------------------");
   DateTimeParts p = DateTime.getParts();
-  Serial.printf("%04d/%02d/%02d %02d:%02d:%02d %ld %+05d\n", p.getYear(),
+  Serial.printf("%04d/%02d/%02d %02d:%02d:%02d %ld (%s)\n", p.getYear(),
                 p.getMonth(), p.getMonthDay(), p.getHours(), p.getMinutes(),
                 p.getSeconds(), p.getTime(), p.getTimeZone());
   Serial.println("--------------------");
-  time_t t = DateTime.now();
-  Serial.println(DateFormatter::format("%Y/%m/%d %H:%M:%S", t));
-  Serial.println(DateFormatter::format("%x - %I:%M %p", t));
-  Serial.println(DateFormatter::format("Now it's %F %I:%M%p.", t));
+  showTime();
 }
 
 void loop() {
-  if (millis() - ms > 5000) {
+  if (millis() - ms > 30000) {
     ms = millis();
     Serial.println("--------------------");
     if (!DateTime.isTimeValid()) {
       Serial.println("Failed to get time from server, retry.");
       DateTime.begin();
     } else {
-      Serial.printf("Up     Time:   %lu seconds\n", millis() / 1000);
-      Serial.printf("Local  Time:   %ld\n", DateTime.now());
-      Serial.printf("Local  Time:   %s\n", DateTime.toString().c_str());
-      Serial.printf("UTC    Time:   %ld\n", DateTime.utcTime());
-      Serial.printf("UTC    Time:   %s\n",
-                    DateTime.formatUTC(DateFormatter::SIMPLE).c_str());
+      showTime();
     }
   }
 }
